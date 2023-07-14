f9494a7285ae355654a9a9daa0d5637204fe4ee0
zafriii <119748214+zafriii@users.noreply.github.com>
2023-07-14 21:42:38 +0600

    Add files via upload

---
+++ task.java
@@ -0,0 +1,53 @@
+class Person
+{
+    public String name;
+    int age;
+    protected String email;
+    
+
+    public void displayinfo1()
+    {
+        System.out.println("Name =" +name);
+        System.out.println("Age =" +age);
+    }
+}
+
+class Teacher extends Person
+{
+    private String password;
+
+    public void displayinfo2()
+    {
+        System.out.println("Name =" +name);
+        System.out.println("Age =" +age);
+        System.out.println("Email =" +email);
+        
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public void setPassword(String pass)
+    {
+        this.password = pass;
+    }
+
+}
+
+public class task {
+public static void main(String[] args) {
+
+    Teacher t1 = new Teacher();
+    t1.name = "Niha";
+    t1.age = 23;
+    t1.email  = "nihazafar@gmail.com";
+    t1.setPassword("678ar");
+    System.out.println("Password="+t1.getPassword());
+
+    t1.displayinfo2();
+
+}   
+
+}
