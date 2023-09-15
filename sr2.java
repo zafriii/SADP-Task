0c339e5b25d8ac00841e40bcea2fdaa366b23d6c
zafriii <119748214+zafriii@users.noreply.github.com>
2023-09-15 21:38:05 +0600

    Add files via upload

---
+++ sr2.java
@@ -0,0 +1,31 @@
+//Single responsibilty 
+
+class Cloth
+{
+    public void stitch(String color, int size)
+    {
+        System.out.println("Stitching a" +" "+color +" "+ "Kurta" +" " +"of size" +" " +size);
+    }
+}
+
+class Food
+
+{
+    public void cook(String name)
+    {
+        System.out.println("Cooking"+" "+name);
+    }
+}
+
+public class sr2 {
+     public static void main(String[] args) {
+
+        Cloth c1 = new Cloth();
+        c1.stitch("Red",36);
+
+        Food f1 = new Food();
+        f1.cook("Pasta");
+
+
+     }
+    }
